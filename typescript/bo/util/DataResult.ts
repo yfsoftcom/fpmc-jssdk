ca7b61417b4f142ffd3d3ce646ac64542d788d1c
wangfan <wangfan@wangfan.wangfan.yunplus.io>
2019-11-03 16:16:42 +0800

    默认使用 time 字段 补充 createAt 的不足

--- typescript/bo/util/DataResult.ts
+++ typescript/bo/util/DataResult.ts
@@ -7,6 +7,14 @@ class DataResult {
   constructor(objectId: ObjectId, data: { [index: string]: any }){
     this.objectId = objectId;
     this.data = data;
+    this.data.createAt = this.data.createAt || ((time) => {
+      if(!time) return 0;
+      if(/\d{13,}/.test(time)){
+        // 时间戳格式的数字
+        return parseInt(`${time}`.substring(0,13));
+      }
+      return 0;
+    })(this.data.time);
   }
 
   get():{ [index: string]: any }{
